546472067e4a820f891b626fe66e7bb8fe731306
subreast <1556841@qq.com>
2023-06-02 15:03:54 +0800

    大量优化

--- model/State.js
+++ model/State.js
@@ -1,19 +1,21 @@
 import os from 'os'
-import lodash from 'lodash'
+import _ from 'lodash'
 import fs from 'fs'
-import { common } from '../lib/index.js'
+import { common } from './index.js'
 import { Config, Data } from '../components/index.js'
+import request from '../lib/request/request.js'
 
-export default new class OSUtils {
-  constructor() {
+export default new class {
+  constructor () {
     this.si = null
+    this.osInfo = null
     // 是否可以获取gpu
     this.isGPU = false
     // 网络
     this._network = null
     // 读写速率
     this._fsStats = null
-
+    // 记录60条数据一分钟记录一次
     this.chartData = {
       network: {
         // 上行
@@ -34,82 +36,104 @@ export default new class OSUtils {
       // 主题
       echarts_theme: Data.readJSON('resources/state/theme_westeros.json')
     }
+
+    this.valueObject = {
+      networkStats: 'rx_sec,tx_sec,iface',
+      currentLoad: 'currentLoad',
+      mem: 'active',
+      fsStats: 'wx_sec,rx_sec'
+    }
+
     this.init()
   }
 
-  set network(value) {
-    if (lodash.isNumber(value[0]?.tx_sec) && lodash.isNumber(value[0]?.rx_sec)) {
+  set network (value) {
+    if (_.isNumber(value[0]?.tx_sec) && _.isNumber(value[0]?.rx_sec)) {
       this._network = value
       this.addData(this.chartData.network.upload, [Date.now(), value[0].tx_sec])
       this.addData(this.chartData.network.download, [Date.now(), value[0].rx_sec])
     }
   }
 
-  get network() {
+  get network () {
     return this._network
   }
 
-  set fsStats(value) {
-    if (lodash.isNumber(value?.wx_sec) && lodash.isNumber(value?.rx_sec)) {
+  set fsStats (value) {
+    if (_.isNumber(value?.wx_sec) && _.isNumber(value?.rx_sec)) {
       this._fsStats = value
       this.addData(this.chartData.fsStats.writeSpeed, [Date.now(), value.wx_sec])
       this.addData(this.chartData.fsStats.readSpeed, [Date.now(), value.rx_sec])
     }
   }
 
-  get fsStats() {
+  get fsStats () {
     return this._fsStats
   }
 
-  async initDependence() {
+  async initDependence () {
     try {
       this.si = await import('systeminformation')
+      this.osInfo = await this.si.osInfo()
       return this.si
     } catch (error) {
       if (error.stack?.includes('Cannot find package')) {
-        logger.warn(`peach-plugin 缺少依赖将无法使用 ${logger.yellow('桃子状态')}`)
+        logger.warn('--------椰奶依赖缺失--------')
+        logger.warn(`yenai-plugin 缺少依赖将无法使用 ${logger.yellow('椰奶状态')}`)
         logger.warn(`如需使用请运行：${logger.red('pnpm add systeminformation -w')}`)
         logger.warn('---------------------------')
         logger.debug(decodeURI(error.stack))
       } else {
-        logger.error(`peach-plugin载入依赖错误：${logger.red('systeminformation')}`)
+        logger.error(`椰奶载入依赖错误：${logger.red('systeminformation')}`)
         logger.error(decodeURI(error.stack))
       }
     }
   }
 
-  async init() {
+  async init () {
     if (!await this.initDependence()) return
+    const { controllers } = await this.si.graphics()
     // 初始化GPU获取
-    if ((await this.si.graphics()).controllers.find(item => item.memoryUsed && item.memoryFree && item.utilizationGpu)) {
+    if (controllers?.find(item =>
+      item.memoryUsed && item.memoryFree && item.utilizationGpu)
+    ) {
       this.isGPU = true
     }
     // 给有问题的用户关闭定时器
-    if (!Config.getWhole.statusTask) return
+    if (!Config.state.statusTask) return
+
+    if (Config.state.statusPowerShellStart) this.si.powerShellStart()
+    this.getData()
     // 网速
-    const networkTimer = setInterval(async () => {
-      this.network = await this.fetchDataWithRetry(this.si.networkStats, networkTimer)
-    }, 5000)
-    // 磁盘写入速度
-    const fsStatsTimer = setInterval(async () => {
-      this.fsStats = await this.fetchDataWithRetry(this.si.fsStats, fsStatsTimer)
-    }, 5000)
-    const memTimer = setInterval(async () => {
-      let { active } = await this.fetchDataWithRetry(
-        this.si.mem, memTimer
-      )
-      if (lodash.isNumber(active)) {
-        this.addData(this.chartData.ram, [Date.now(), active])
+    const Timer = setInterval(async () => {
+      let data = await this.getData()
+      if (_.isEmpty(data)) clearInterval(Timer)
+    }, 60000)
+  }
+
+  async getData () {
+    let data = await this.si.get(this.valueObject)
+    _.forIn(data, (value, key) => {
+      if (_.isEmpty(value)) {
+        logger.debug(`获取${key}数据失败，停止获取对应数据`)
+        delete this.valueObject[key]
       }
-    }, 5000)
-    const cpuTimer = setInterval(async () => {
-      let { currentLoad } = await this.fetchDataWithRetry(
-        this.si.currentLoad, cpuTimer
-      )
-      if (lodash.isNumber(currentLoad)) {
-        this.addData(this.chartData.cpu, [Date.now(), currentLoad])
-      }
-    }, 5000)
+    })
+    let {
+      fsStats,
+      networkStats,
+      mem: { active },
+      currentLoad: { currentLoad }
+    } = data
+    this.fsStats = fsStats
+    this.network = networkStats
+    if (_.isNumber(active)) {
+      this.addData(this.chartData.ram, [Date.now(), active])
+    }
+    if (_.isNumber(currentLoad)) {
+      this.addData(this.chartData.cpu, [Date.now(), currentLoad])
+    }
+    return data
   }
 
   /**
@@ -117,14 +141,14 @@ export default new class OSUtils {
    *
    * @param {Array} arr - 要添加数据的数组
    * @param {*} data - 要添加的新数据
-   * @param {number} [maxLen=50] - 数组允许的最大长度，默认值为50
+   * @param {number} [maxLen=60] - 数组允许的最大长度，默认值为60
    * @returns {void}
    */
-  addData(arr, data, maxLen = 50) {
+  addData (arr, data, maxLen = 60) {
     if (data === null || data === undefined) return
     // 如果数组长度超过允许的最大值，删除第一个元素
     if (arr.length >= maxLen) {
-      lodash.pullAt(arr, 0)
+      _.pullAt(arr, 0)
     }
     // 添加新数据
     arr.push(data)
@@ -133,17 +157,18 @@ export default new class OSUtils {
   /**
   * 重试获取数据，直到成功或达到最大重试次数。
   * @param {Function} fetchFunc 获取数据的函数，返回一个Promise对象。
+  * @param {Array} [params=[]] 需要执行函数的参数数组
   * @param {Number} [timerId] 定时器的id，用于在获取数据失败时停止定时器
   * @param {Number} [maxRetryCount=3] 最大重试次数。
   * @param {Number} [retryInterval=1000] 两次重试之间的等待时间，单位为毫秒。。
   * @return {Promise} 获取到的数据。如果达到最大重试次数且获取失败，则返回null。
   */
-  async fetchDataWithRetry(fetchFunc, timerId, maxRetryCount = 3, retryInterval = 1000) {
+  async fetchDataWithRetry (fetchFunc, params = [], timerId, maxRetryCount = 3, retryInterval = 1000) {
     let retryCount = 0
     let data = null
     while (retryCount <= maxRetryCount) {
-      data = await fetchFunc()
-      if (!lodash.isEmpty(data)) {
+      data = await fetchFunc(...params)
+      if (!_.isEmpty(data)) {
         break
       }
       retryCount++
@@ -165,7 +190,7 @@ export default new class OSUtils {
   * @param {boolean} [isSuffix=true] - 如果为 true，则在所得到的大小后面加上 kb、mb、gb、tb 等后缀.
   * @returns {string} 文件大小格式转换后的字符串.
   */
-  getFileSize(size, isByte = true, isSuffix = true) { // 把字节转换成正常文件大小
+  getFileSize (size, isByte = true, isSuffix = true) { // 把字节转换成正常文件大小
     if (size == null || size == undefined) return 0
     let num = 1024.00 // byte
     if (isByte && size < num) {
@@ -188,13 +213,13 @@ export default new class OSUtils {
     * @param {Number} res 百分比小数
     * @return {*} css样式
   */
-  Circle(res) {
+  Circle (res) {
     let num = (res * 360).toFixed(0)
-    let color = '#90ee90'
+    let color = 'var(--low-color)'
     if (res >= 0.9) {
-      color = '#d73403'
+      color = 'var(--high-color)'
     } else if (res >= 0.8) {
-      color = '#ffa500'
+      color = 'var(--medium-color)'
     }
     let leftCircle = `style="transform:rotate(-180deg);background:${color};"`
     let rightCircle = `style="transform:rotate(360deg);background:${color};"`
@@ -207,7 +232,7 @@ export default new class OSUtils {
   }
 
   /** 获取nodejs内存情况 */
-  getNodeInfo() {
+  getNodeInfo () {
     let memory = process.memoryUsage()
     // 总共
     let rss = this.getFileSize(memory.rss)
@@ -219,7 +244,7 @@ export default new class OSUtils {
     let occupy = (memory.rss / (os.totalmem() - os.freemem())).toFixed(2)
     return {
       ...this.Circle(occupy),
-      inner: parseInt(occupy * 100) + '%',
+      inner: Math.round(occupy * 100) + '%',
       title: 'Node',
       info: [
         `总 ${rss}`,
@@ -230,7 +255,7 @@ export default new class OSUtils {
   }
 
   /** 获取当前内存占用 */
-  getMemUsage() {
+  getMemUsage () {
     // 内存使用率
     let MemUsage = (1 - os.freemem() / os.totalmem()).toFixed(2)
     // 空闲内存
@@ -242,7 +267,7 @@ export default new class OSUtils {
 
     return {
       ...this.Circle(MemUsage),
-      inner: parseInt(MemUsage * 100) + '%',
+      inner: Math.round(MemUsage * 100) + '%',
       title: 'RAM',
       info: [
         `总共 ${totalmem}`,
@@ -253,41 +278,50 @@ export default new class OSUtils {
   }
 
   /** 获取CPU占用 */
-  async getCpuInfo(arch) {
-    // cpu使用率
-    let cpu_info = (await this.si.currentLoad())?.currentLoad
-    if (cpu_info == null || cpu_info == undefined) return false
+  async getCpuInfo () {
+    let { currentLoad: { currentLoad }, cpuCurrentSpeed } = await this.si.get({
+      currentLoad: 'currentLoad',
+      cpuCurrentSpeed: 'max,avg'
+    })
+    if (currentLoad == null || currentLoad == undefined) return false
     // 核心
-    let hx = os.cpus()
+    let cores = os.cpus()
     // cpu制造者
-    let cpumodel = hx[0]?.model.slice(0, hx[0]?.model.indexOf(' ')) || ''
-    // 最大MHZ
-    let maxspeed = await this.si.cpuCurrentSpeed()
-
+    let cpuModel = cores[0]?.model.slice(0, cores[0]?.model.indexOf(' ')) || ''
     return {
-      ...this.Circle(cpu_info / 100),
-      inner: `${parseInt(cpu_info)}%`,
+      ...this.Circle(currentLoad / 100),
+      inner: Math.round(currentLoad) + '%',
       title: 'CPU',
       info: [
-        `${cpumodel} ${hx.length}核 ${arch}`,
-        `平均${maxspeed.avg}GHz`,
-        `最大${maxspeed.max}GHz`
+        `${cpuModel} ${cores.length}核 ${this.osInfo?.arch}`,
+        `平均${cpuCurrentSpeed.avg}GHz`,
+        `最大${cpuCurrentSpeed.max}GHz`
       ]
 
     }
   }
 
   /** 获取GPU占用 */
-  async getGPU() {
+  async getGPU () {
     if (!this.isGPU) return false
     try {
-      let graphics = (await this.si.graphics()).controllers.find(item => item.memoryUsed && item.memoryFree && item.utilizationGpu)
-      let { vendor, temperatureGpu, utilizationGpu, memoryTotal, memoryUsed, powerDraw } = graphics
-      temperatureGpu = temperatureGpu ? temperatureGpu + '℃' : ''
-      powerDraw = powerDraw ? powerDraw + 'W' : ''
+      const { controllers } = await this.si.graphics()
+      let graphics = controllers?.find(item =>
+        item.memoryUsed && item.memoryFree && item.utilizationGpu
+      )
+      if (!graphics) {
+        logger.warn('[Yenai-plugin][state]状态GPU数据异常：\n', controllers)
+        return false
+      }
+      let {
+        vendor, temperatureGpu, utilizationGpu,
+        memoryTotal, memoryUsed, powerDraw
+      } = graphics
+      temperatureGpu && (temperatureGpu = temperatureGpu + '℃')
+      powerDraw && (powerDraw = powerDraw + 'W')
       return {
         ...this.Circle(utilizationGpu / 100),
-        inner: parseInt(utilizationGpu) + '%',
+        inner: Math.round(utilizationGpu) + '%',
         title: 'GPU',
         info: [
           `${vendor} ${temperatureGpu} ${powerDraw}`,
@@ -296,7 +330,7 @@ export default new class OSUtils {
         ]
       }
     } catch (e) {
-      console.log(e)
+      logger.warn('[Yenai-Plugin][State] 获取GPU失败')
       return false
     }
   }
@@ -305,33 +339,34 @@ export default new class OSUtils {
    * @description: 获取硬盘
    * @return {*}
    */
-  async getfsSize() {
+  async getFsSize () {
     // 去重
-    let HardDisk = lodash.uniqWith(await this.si.fsSize(),
-      (a, b) => a.used === b.used && a.size === b.size && a.use === b.use && a.available === b.available)
-    // 过滤
-    HardDisk = HardDisk.filter(item => item.size && item.used && item.available && item.use)
+    let HardDisk = _.uniqWith(await this.si.fsSize(),
+      (a, b) =>
+        a.used === b.used && a.size === b.size && a.use === b.use && a.available === b.available
+    )
+      .filter(item => item.size && item.used && item.available && item.use)
     // 为空返回false
-    if (lodash.isEmpty(HardDisk)) return false
+    if (_.isEmpty(HardDisk)) return false
     // 数值转换
     return HardDisk.map(item => {
       item.used = this.getFileSize(item.used)
       item.size = this.getFileSize(item.size)
-      item.use = Math.ceil(item.use)
-      item.color = '#90ee90'
+      item.use = Math.round(item.use)
+      item.color = 'var(--low-color)'
       if (item.use >= 90) {
-        item.color = '#d73403'
+        item.color = 'var(--high-color)'
       } else if (item.use >= 70) {
-        item.color = '#ffa500'
+        item.color = 'var(--medium-color)'
       }
       return item
     })
   }
 
   /** 获取FastFetch */
-  async getFastFetch(e) {
+  async getFastFetch (e) {
     if (process.platform == 'win32' && !/pro/.test(e.msg)) return ''
-    let ret = await common.execSync('bash plugins/peach-plugin/resources/state/state.sh')
+    let ret = await common.execSync('bash plugins/yenai-plugin/resources/state/state.sh')
     if (ret.error) {
       e.reply(`❎ 请检查是否使用git bash启动Yunzai-bot\n错误信息：${ret.stderr}`)
       return ''
@@ -340,8 +375,13 @@ export default new class OSUtils {
   }
 
   // 获取读取速率
-  get DiskSpeed() {
-    if (!this.fsStats || this.fsStats.rx_sec == null || this.fsStats.wx_sec == null) return false
+  get DiskSpeed () {
+    if (!this.fsStats ||
+      this.fsStats.rx_sec == null ||
+      this.fsStats.wx_sec == null
+    ) {
+      return false
+    }
     return {
       rx_sec: this.getFileSize(this.fsStats.rx_sec, false, false),
       wx_sec: this.getFileSize(this.fsStats.wx_sec, false, false)
@@ -350,22 +390,27 @@ export default new class OSUtils {
 
   /**
    * @description: 获取网速
-   * @return {*}
+   * @return {object}
    */
-  get getnetwork() {
-    let network = {}
-    try { network = lodash.cloneDeep(this.network)[0] } catch { return false }
-    if (network.rx_sec == null || network.tx_sec == null) return false
+  get getnetwork () {
+    let network = _.cloneDeep(this.network)?.[0]
+    if (!network || network.rx_sec == null || network.tx_sec == null) {
+      return false
+    }
     network.rx_sec = this.getFileSize(network.rx_sec, false, false)
     network.tx_sec = this.getFileSize(network.tx_sec, false, false)
-    return network
+    // return network
+    return {
+      first: network.iface,
+      tail: `↑${network.tx_sec}/s | ↓${network.rx_sec}/s`
+    }
   }
 
   /**
  * @description: 取插件包
  * @return {*} 插件包数量
  */
-  get numberOfPlugIns() {
+  get getPluginNum () {
     let str = './plugins'
     let arr = fs.readdirSync(str)
     let plugin = []
@@ -377,10 +422,54 @@ export default new class OSUtils {
     })
     let del = ['example', 'genshin', 'other', 'system', 'bin']
     plugin = plugin.filter(item => !del.includes(item))
-
+    const plugins = plugin?.length || 0
+    const js = fs.readdirSync('./plugins/example')?.filter(item => item.includes('.js'))?.length || 0
+    // return {
+    //   plugins: plugin?.length || 0,
+    //   js: fs.readdirSync('./plugins/example')?.filter(item => item.includes('.js'))?.length || 0
+    // }
     return {
-      plugins: plugin?.length || 0,
-      js: fs.readdirSync('./plugins/example')?.filter(item => item.includes('.js'))?.length || 0
+      first: '插件',
+      tail: `${plugins} plugin | ${js} js`
+    }
+  }
+
+  async getNetworkLatency (url, timeoutTime = 5000) {
+    const AbortController = globalThis.AbortController || await import('abort-controller')
+
+    const controller = new AbortController()
+    const timeout = setTimeout(() => {
+      controller.abort()
+    }, timeoutTime)
+    try {
+      const startTime = Date.now()
+      let { status } = await request.get(url, { signal: controller.signal })
+      const endTime = Date.now()
+      let delay = endTime - startTime
+      let color = ''; let statusColor = ''
+      if (delay > 2000) {
+        color = '#F44336'
+      } else if (delay > 500) {
+        color = '#d68100'
+      } else {
+        color = '#188038'
+      }
+      if (status >= 500) {
+        statusColor = '#9C27B0'
+      } else if (status >= 400) {
+        statusColor = '#F44336'
+      } else if (status >= 300) {
+        statusColor = '#FF9800'
+      } else if (status >= 200) {
+        statusColor = '#188038'
+      } else if (status >= 100) {
+        statusColor = '#03A9F4'
+      }
+      return `<span style='color:${statusColor}'>${status}</span> | <span style='color:${color}'>${delay}ms</span>`
+    } catch {
+      return "<span style='color:#F44336'>timeout</span>"
+    } finally {
+      clearTimeout(timeout)
     }
   }
 }()
